b5be0e0b9d387eb9ba48621b4e120a2e16bcf217
mariiamel <mariiamelnikova96@gmail.com>
2021-07-27 22:10:29 +0500

    w/o repeat solved

--- withoutRepeat.js
+++ withoutRepeat.js
@@ -1,10 +1,10 @@
 const arr = [1,1,2,2,3,4,4,5,6,6,7,7,8,8,9]
 
-function withoutRepeat(arr){
+function withoutRepeat(array){
     const result = []
     const obj = {}
 
-    for (let i = 0; i <= arr.length; i++){
+    for (let i = 0; i < arr.length; i++){
         let current = arr[i]
         if (!(current in obj)){
             obj[current] = 1;
@@ -21,4 +21,4 @@ function withoutRepeat(arr){
     return result;
 }
 
-withoutRepeat(arr)
+console.log(withoutRepeat(arr))
