63a06935cf2b6cb27c85bb28f642768860ee3835
mariiamel <mariiamelnikova96@gmail.com>
2021-10-05 13:40:27 +0500

    added mergeTwoSortedLists

---
+++ challenges/mergeTwoSortedLists.js
@@ -0,0 +1,19 @@
+
+// O(n+m) time
+// O(n+m) space
+
+var mergeTwoLists = function(l1, l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
+
+    if(l1.val <= l2.val){
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }else{
+        l2.next = mergeTwoLists(l2.next, l1);
+        return l2;
+    }
+};
+
+
+mergeTwoLists([1,2,4], [1,3,4])
