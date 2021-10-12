5619b02166c207356b6e9702ec88ea05946ccc2c
mariiamel <mariiamelnikova96@gmail.com>
2021-10-12 17:49:22 +0500

    solved removeDuplicates and FizzBuzz

---
+++ challenges/fizzBuzz.js
@@ -0,0 +1,34 @@
+
+//Leetcode
+var fizzBuzz = function(n) {
+    const answer = [];
+    for(let i = 1; i <= n; i++){
+        let curr = i;
+        if(curr % 15 === 0){
+           answer.push("FizzBuzz"); 
+        } else if(curr % 3 === 0){
+          answer.push("Fizz");  
+        } else if(curr % 5 === 0){
+          answer.push("Buzz");  
+        } else {
+            answer.push(`${curr}`);
+        }
+    }
+    return answer;
+};
+
+console.log(fizzBuzz(3))// => ["1","2","Fizz"]
+
+
+//Classic
+for (let i = 1; i <= 100; i++) {
+    if(i % 15 === 0){
+        console.log('FizzBuzz');
+    }else if(i % 3 === 0){
+        console.log('Fizz');
+    } else if(i % 5 === 0 ){
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
---
+++ challenges/removeDuplicates.js
@@ -0,0 +1,17 @@
+
+var removeDuplicates = function(nums) {
+    if(nums.length === 1) return nums.length;
+    
+    for(let i = 1; i < nums.length; i++){
+        const prevValue = nums[i-1];
+        const currentValue = nums[i];
+        
+        if(prevValue === currentValue){
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+};
+
+console.log(removeDuplicates([3]))
