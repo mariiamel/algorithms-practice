1bac16feacd7cfbf017eb4f8b657045bf5df137d
mariiamel <mariiamelnikova96@gmail.com>
2022-08-16 22:49:50 -0700

    palindrome Number

---
+++ challenges/palindromeNumber.js
@@ -0,0 +1,22 @@
+
+
+const palindromeNumber = (num) => {
+    if (num < 0) return false;
+
+    const number = num.toString().split('');
+
+    let first = 0,
+        last = number.length - 1;
+    
+    while (first < last) {
+        if (number[first] !== number[last]) {
+            return false;
+        }
+        first++;
+        last--;
+    }
+    return true;
+}
+
+console.log(palindromeNumber(121)); // true
+console.log(palindromeNumber(-121)); // false
